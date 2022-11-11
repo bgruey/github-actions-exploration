35ae0f2a7b0ae01717ae74eae9361a1afc3eed6b
Ben Gruey <bgruey@recruiter.com>
2022-11-11 16:18:30 -0600

    fix chain of commits

--- python_pytest_import_in_subprocess/test/sub_test/subprocess_script.py
+++ python_pytest_import_in_subprocess/test/sub_test/subprocess_script.py
@@ -1,13 +1,16 @@
 import logging
+import os
 import sys
-
-from test.sub_test.sub_test_lib import sub_test_string
 
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.DEBUG)
-
 log_handler = logging.StreamHandler(sys.stdout)
 log_handler.setLevel(logging.DEBUG)
-
 root_logger.addHandler(log_handler)
+
+logging.info("PATH: %s", sys.path)
+logging.info("CWD: %s", os.getcwd())
+logging.info("FILE: %s", os.path.dirname(__file__))
+
+from test.sub_test.sub_test_lib import sub_test_string
 logging.info(sub_test_string)
